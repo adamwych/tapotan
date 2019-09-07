d869e3c6a87f89a6b0d1b2cedf751c7fb23bb84f
szuli <shoolygl@gmail.com>
2019-09-07 04:21:32 +0200

    Added methods to change physics body's material, collision group and collision mask.

--- src/world/components/GameObjectComponentPhysicsBody.ts
+++ src/world/components/GameObjectComponentPhysicsBody.ts
@@ -5,8 +5,15 @@ import World from '../World';
 export default class GameObjectComponentPhysicsBody extends GameObjectComponent {
 
     protected body: p2.Body;
+    protected shape: p2.Shape;
 
+    /**
+     * 
+     * @param bodyOptions 
+     * @param shape 
+     */
     public initialize(bodyOptions: p2.BodyOptions, shape: p2.Shape): void {
+        this.shape = shape;
         this.body = new p2.Body({
             ...bodyOptions,
             id: this.gameObject.getId()
@@ -14,18 +21,17 @@ export default class GameObjectComponentPhysicsBody extends GameObjectComponent 
         
         this.body.addShape(shape);
 
-        /*shape.material = PhysicsMaterials.Ground;
-        shape.collisionGroup = PhysicsBodyCollisionGroup.Block;
-        shape.collisionMask = PhysicsBodyCollisionGroup.Entity;
-
-        if (canPlayerCollide) {
-            shape.collisionMask |= PhysicsBodyCollisionGroup.Player;
-        }*/
-
         this.gameObject.getWorld().addPhysicsBody(this.gameObject, this.body);
         this.gameObject.on('transform.scaleChanged', this.handleTransformScaleChanged);
     }
 
+    /**
+     * Creates a rectangular body of specified dimensions.
+     * 
+     * @param width Width in world scale.
+     * @param height Height in world scale.
+     * @param options 
+     */
     public initializeBox(width: number, height: number, options: p2.BodyOptions) {
         this.initialize(options, new p2.Box({
             width: width * World.PHYSICS_SCALE,
@@ -38,16 +44,47 @@ export default class GameObjectComponentPhysicsBody extends GameObjectComponent 
         this.gameObject.off('transform.scaleChanged', this.handleTransformScaleChanged);
     }
 
+    public setMaterial(material: p2.Material) {
+        this.shape.material = material;
+    }
+
+    public getMaterial() {
+        return this.shape.material;
+    }
+
+    public setCollisionGroup(collisionGroup: number) {
+        this.shape.collisionGroup = collisionGroup;
+    }
+
+    public getCollisionGroup(): number {
+        return this.shape.collisionGroup;
+    }
+
+    public setCollisionMask(collisionMask: number) {
+        this.shape.collisionMask = collisionMask;
+    }
+
+    public getCollisionMask(): number {
+        return this.shape.collisionMask;
+    }
+
     private handleTransformScaleChanged = (scaleX: number, scaleY: number) => {
         // Re-create the body if the bounds have changed.
-        // TODO        
+        // TODO
     }
 
     /**
      * Returns the physical body.
      */
-    public getBody() {
+    public getBody(): p2.Body {
         return this.body;
+    }
+
+    /**
+     * Returns shape of the physical body.
+     */
+    public getShape(): p2.Shape {
+        return this.shape;
     }
 
 }
