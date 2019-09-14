722ec211b2eedd277f2da8e2513bbaebc2839471
szuli <shoolygl@gmail.com>
2019-09-14 09:32:21 +0200

    Made coins smaller.

--- src/world/prefabs/environment/PrefabEnvironmentCoin.ts
+++ src/world/prefabs/environment/PrefabEnvironmentCoin.ts
@@ -3,6 +3,7 @@ import SpritesheetAnimatorTimer from "../../../graphics/SpritesheetAnimatorTimer
 import CollectableCategory from "../../CollectableCategory";
 import GameObjectComponentAnimator from "../../components/GameObjectComponentAnimator";
 import GameObjectComponentCollectable from "../../components/GameObjectComponentCollectable";
+import GameObjectComponentTransform from "../../components/GameObjectComponentTransform";
 import GameObject from "../../GameObject";
 import PhysicsBodyCollisionGroup from "../../physics/PhysicsBodyCollisionGroup";
 import World from "../../World";
@@ -13,12 +14,23 @@ import { PrefabBasicProps } from "../Prefabs";
 const coinAnimationTimer = new SpritesheetAnimatorTimer();
 
 export default createPrefabSpawnFunction('environment_coin', (gameObject: GameObject, world: World, props: PrefabBasicProps) => {
+
+    const gameObject2 = new GameObject();
+    gameObject2.setWorld(world);
+
     const texture = world.getTileset().getResourceById('environment_coin_animation').texture;
 
-    const animatorComponent = gameObject.createComponent<GameObjectComponentAnimator>(GameObjectComponentAnimator);
+    const animatorComponent = gameObject2.createComponent<GameObjectComponentAnimator>(GameObjectComponentAnimator);
     animatorComponent.initialize(coinAnimationTimer);
-    animatorComponent.addAnimation('animation', new Spritesheet(texture, 16, 16), 150);
+    animatorComponent.addAnimation('animation', new Spritesheet(texture, 32, 32), 150);
+    animatorComponent.setCellWidth(32);
+    animatorComponent.setCellHeight(32);
+    animatorComponent.setTransformMultiplier(2);
     animatorComponent.playAnimation('animation');
+
+    gameObject2.createComponent(GameObjectComponentTransform);
+    gameObject2.transformComponent.setScale(0.5, 0.5);
+    gameObject.addChild(gameObject2);
 
     createPrefabDefaultTransform(gameObject, props, {
         mass: 0,
@@ -28,6 +40,5 @@ export default createPrefabSpawnFunction('environment_coin', (gameObject: GameOb
     }, PhysicsBodyCollisionGroup.Collectable);
 
     gameObject.setCustomProperty('sensor', true);
-
     gameObject.createComponent<GameObjectComponentCollectable>(GameObjectComponentCollectable).initialize(CollectableCategory.Coin);
 });
