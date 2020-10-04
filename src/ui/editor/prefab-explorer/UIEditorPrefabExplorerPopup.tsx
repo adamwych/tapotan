eede782a6866b6258bbe70b4779480ec099f4c9e
szuli <shoolygl@gmail.com>
2020-10-05 01:29:22 +0200

    Fixed scaling prefab preview images if their height was higher than their width.

--- src/ui/editor/prefab-explorer/UIEditorPrefabExplorerPopup.tsx
+++ src/ui/editor/prefab-explorer/UIEditorPrefabExplorerPopup.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import getBundledResourceAsDataURL from '../../lib/getBundledResourceAsDataURL';
-import TilesetEditorCategory from '../../../world/tileset/TilesetEditorCategory';
-import Tapotan from '../../../core/Tapotan';
-import getTilesetResourceAsDataURL from '../../lib/getTilesetResourceAsDataURL';
 import CustomScrollbars from 'react-custom-scrollbars';
+import Tapotan from '../../../core/Tapotan';
+import TilesetEditorCategory from '../../../world/tileset/TilesetEditorCategory';
+import getBundledResourceAsDataURL from '../../lib/getBundledResourceAsDataURL';
+import getTilesetResourceAsDataURL from '../../lib/getTilesetResourceAsDataURL';
 
 interface UIEditorPrefabExplorerPopupProps {
     category: TilesetEditorCategory;
@@ -14,6 +14,18 @@ interface UIEditorPrefabExplorerPopupProps {
 
 export default function UIEditorPrefabExplorerPopup(props: UIEditorPrefabExplorerPopupProps) {
     const tileset = Tapotan.getInstance().getGameManager().getWorld().getTileset();
+
+    const handleImgRef = React.useCallback((element: HTMLImageElement) => {
+        if (!element) {
+            return;
+        }
+
+        setTimeout(() => {
+            if (element.height > element.width) {
+                element.parentElement.classList.add('editor-prefab-explorer-grid-group-item--invert');
+            }
+        });
+    }, []);
 
     return (
         <div className="editor-prefab-explorer-popup" style={{ backgroundImage: getBundledResourceAsDataURL('Graphics/Editor/PrefabSelectorBackground.svg') }}>
@@ -31,7 +43,7 @@ export default function UIEditorPrefabExplorerPopup(props: UIEditorPrefabExplore
                                 <div className="editor-prefab-explorer-grid-group-items">
                                     {group.resources.map((resource, resourceIndex) => (
                                         <div key={resourceIndex} className="editor-prefab-explorer-grid-group-item" onClick={() => props.onItemClick(resource)}>
-                                            <img src={getTilesetResourceAsDataURL(tileset, resource, false)} />
+                                            <img src={getTilesetResourceAsDataURL(tileset, resource, false)} ref={handleImgRef} />
                                         </div>
                                     ))}
                                 </div>
